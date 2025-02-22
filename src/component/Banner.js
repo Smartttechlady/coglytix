3944550578d9cab4175cbeff3a004964d4d6b0c7
ibukunodofin <odofinibukun123@gmail.com>
2025-02-22 05:20:01 +0100

    Update

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Banner.css'
 import { Link } from 'react-router-dom'
+import robot from "./../images/Robot2.gif";
 
 
 function Banner() {
@@ -13,10 +14,10 @@ function Banner() {
               <h1 className="fw-bold">Pioneering AI & Data Innovations</h1>
               <h2>Transforming industries with intelligence</h2>
               <p>Coglytix is at the forefront of AI, Data, and Robotics, delivering cutting-edge solutions for startups, enterprises, and institutions.</p>
-              <Link href="#products" className="btn btn-gradient hero-btn">Explore Our Products</Link>
+              <Link to="*" className="btn btn-gradient hero-btn">Explore Our Products</Link>
             </div>
             <div className="col-lg-6 text-center">
-              <img src= {("./..//images/hero img2.png")} alt="Tech Illustration" className="img-fluid hero-img" data-aos="flip-left" data-aos-duration="3000"/>
+              <img src= {robot} alt="Tech Illustration" className="img-fluid hero-img" data-aos="flip-left" data-aos-duration="3000"/>
             </div>
           </div>
         </div>
